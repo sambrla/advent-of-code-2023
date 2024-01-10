f9ceb316da3b318ca0c4c96501f8bd8021e08d38
sambrla <sambrla@users.noreply.github.com>
2024-01-10 11:13:35 +1030

    Solution for Day 13.2

---
+++ day13/day13.2.py
@@ -0,0 +1,70 @@
+"""
+Advent of Code 2023
+Day 13.2
+"""
+
+import sys
+
+
+def reflect(lines: list[str], l: int, r: int) -> tuple[int, int]:
+    """
+    Returns tuple of reflected line count as (above, below)
+    """
+    start_l = l
+    start_r = r
+    smudges = 0
+
+    # Check reflected lines
+    while l >= 0 and r < len(lines):
+        for c in range(len(lines[l])):
+            if lines[l][c] != lines[r][c]:
+                smudges += 1
+            # There should only be one smudge
+            if smudges > 1:
+                break
+        l -= 1
+        r += 1
+
+    return (start_l+1, len(lines)-start_r) if smudges == 1 else (0, 0)
+
+
+def find_reflections(lines: list[str]) -> tuple[int, int]:
+    res = (0, 0)
+    i = 0
+
+    # Find lines of reflection
+    while i < len(lines)-1:
+        # Check all line pairs
+        res = reflect(lines, i, i+1)
+        if res != (0, 0):
+            break
+        i += 1
+
+    return res
+
+
+def main():
+    if len(sys.argv) < 2:
+        sys.exit("Usage: {} <input>".format(sys.argv[0]))
+
+    with open(sys.argv[1]) as doc:
+        patterns = doc.read()
+        patterns = patterns.split("\n\n")
+        patterns = [[line for line in p.split("\n") if line] for p in patterns]
+
+        ans = 0
+        for p in patterns:
+            # Horizontal
+            up, _ = find_reflections(p)
+            ans += 100 * up
+
+            # Vertical
+            t = ["".join(reversed(l)) for l in zip(*p)]
+            left, _ = find_reflections(t)
+            ans += left
+
+        print(ans)
+
+
+if __name__ == "__main__":
+    main()
